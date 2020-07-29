db24b24a28547557ed671f024ed0ef83305706fe
Waves <64664814+sanders43@users.noreply.github.com>
2020-07-29 17:00:50 -0500

    modular and require

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = profileDataArr => {
-    for (let i= 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
 
-        profileDataArr.forEach(profileItem => console.log(profileItem));
-        
-    }
-};
-
-printProfileData(profileDataArgs);
-
+fs.writeFile('index.html', generatePage(name,github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
