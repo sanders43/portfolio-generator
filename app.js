d42fdeac337efb4f37c714ad346b19b33d51f450
Waves <64664814+sanders43@users.noreply.github.com>
2020-07-29 17:48:23 -0500

    inquirer

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
+//const profileDataArgs = process.argv.slice(2);
+//const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('index.html', generatePage(name,github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
+//fs.writeFile('index.html', generatePage(name,github), err => {
+   // if (err) throw err;
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+//})
+
+const inquirer = require ('inquirer')
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your name?"
+    }
+])
+.then(answers => console.log(answers));
