dc907209b4cf0b32d90a4d3eb7c054ab2c88febe
Waves <64664814+sanders43@users.noreply.github.com>
2020-07-30 22:06:05 -0500

    html generation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 //const profileDataArgs = process.argv.slice(2);
 //const [name, github] = profileDataArgs;
 
@@ -136,7 +136,13 @@ const promptProject = (portfolioData) => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
-})
+     const pageHTML = generatePage(portfolioData);
 
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
+  });
+  
 
